8c31df6390cbb13f412dd09617ffa0bcdcd17d3a
b-612 <fromtheorion@yandex.ru>
2020-02-07 18:59:22 +0300

    Дебажит открытие меню

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,6 +1,13 @@
 'use strict';
 
 (function () {
+  var SWIPE_VAL = 20;
+
+  var touchX = {
+    touchStartX: 0,
+    touchEndX: 0
+  };
+
   var $body = $('body');
   var $mainNav = $body.find('.nav');
   var $navList = $mainNav.find('.nav__list');
@@ -11,6 +18,7 @@
   var onToggleClick = function () {
     $mainNav.toggleClass('nav--opened');
     $body.toggleClass('body-anim-on');
+    $body.attr('style', 'overflow-x: hidden');
     $navList.toggleClass('nav__list--animation-on');
 
     if (onToggleClick.isClicked) {
@@ -19,6 +27,8 @@
       window.setTimeout(function () {
         $body.removeClass('body-anim-off');
         $navList.removeClass('nav__list--animation-off');
+        $body.removeAttr('style');
+        $body.removeAttr('class');
         onToggleClick.isClicked = false;
       }, 501);
     }
@@ -29,6 +39,28 @@
 
     if (!onToggleClick.isClicked) {
       onToggleClick.isClicked = true;
+    }
+  });
+
+  var onSwipe = function (evt) {
+    if ($mainNav.hasClass('nav--opened') && touchX.touchStartX > touchX.touchEndX) {
+      onToggleClick();
+      onToggleClick.isClicked = false;
+    } else if (!$mainNav.hasClass('nav--opened') && touchX.touchStartX < touchX.touchEndX  && touchX.touchStartX <= 40) {
+      onToggleClick();
+      onToggleClick.isClicked = true;
+    }
+  };
+
+  $body.on('touchstart', function (evt) {
+    touchX.touchStartX = evt.touches[0].screenX;
+  });
+
+  $body.on('touchend', function (evt) {
+    touchX.touchEndX = evt.changedTouches[0].screenX;
+
+    if (touchX.touchEndX - touchX.touchStartX > SWIPE_VAL || touchX.touchStartX - touchX.touchEndX > SWIPE_VAL) {
+      onSwipe(evt);
     }
   });
 })();
